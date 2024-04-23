d2bc9e51966983ba4be985d1d7938284c92bbaca
bryannuar <bryannuar00@gmail.com>
2024-04-24 01:32:53 +0800

    fixed background for auth pages

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -153,7 +153,7 @@ export default {
 
 <style scoped>
 .background {
-  background: url("..\\assets\\bg2.png") no-repeat center center fixed;
+  background: url("../assets/bg2.png") no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
